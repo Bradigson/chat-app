137e03b00195715d47da1250ca906e2799f97823
Bradigson <bradigson@gmail.com>
2022-05-02 15:43:02 -0400

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import '../assets/styles/Header.scss';
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = ()=>{
     const [menu, setMenu] = useState(false);
@@ -13,6 +14,76 @@ const Header = ()=>{
                     <div className='line line-1'></div>
                     <div className='line line-2'></div>
                     <div className='line line-3'></div>
+                </div>
+                
+                <div className='users-container'>
+                    <div className='header-users'>
+                        <div>
+                            <h2>User aname</h2>
+                        </div>
+                        <div className='user-phoro'>
+
+                        </div>
+                    </div>
+
+                    <div className='search'>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text text-muted" id="basic-addon1"><i className='bx bx-search-alt-2' ></i></span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                        </div>
+                        
+                    </div>
+
+                    <div className='users'>
+                        <ul>
+                            <li>
+                                <NavLink to='#'className='nav-link'>
+                                    <div className='logo-user'>
+                                        <h4>B</h4>
+                                    </div>
+                                    <div>
+                                        <h3>User name</h3>
+                                        <span className='text-dark text-muted'>online</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='#'className='nav-link'>
+                                    <div className='logo-user'>
+                                        <h4>B</h4>
+                                    </div>
+                                    <div>
+                                        <h3>User name</h3>
+                                        <span className='text-dark text-muted'>offline</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='#'className='nav-link'>
+                                    <div className='logo-user'>
+                                        <h4>B</h4>
+                                    </div>
+                                    <div>
+                                        <h3>User name</h3>
+                                        <span className='text-dark text-muted'>online</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='#'className='nav-link'>
+                                    <div className='logo-user'>
+                                        <h4>B</h4>
+                                    </div>
+                                    <div>
+                                        <h3>User name</h3>
+                                        <span className='text-dark text-muted'>offline</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+                            
+                          
+                        </ul>
+                    </div>
                 </div>
             </nav>
     )
