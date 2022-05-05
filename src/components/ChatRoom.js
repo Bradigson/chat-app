3f87869f0299360a429be6166a5bceab16354284
Bradigson <bradigson@gmail.com>
2022-05-05 11:34:18 -0400

    input updated

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -196,7 +196,7 @@ const ChatRoom = ()=>{
                             <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
                         </svg>
                     </button>
-                    <input autoFocus type="text" className="form-control" placeholder="type your massage here..." aria-label="Username" aria-describedby="addon-wrapping" value={mensaje} onChange={handleMessage}/>
+                    <input  type="text" className="form-control" placeholder="type your massage here..." aria-label="Username" aria-describedby="addon-wrapping" value={mensaje} onChange={handleMessage}/>
                     <button className='btn' ><i className='bx bxl-telegram'></i></button>
                     
                 </form>
